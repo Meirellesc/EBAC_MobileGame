e36ce66337f15456613b36c71b2aa62c2555f5cc
Lucas Meirelles <lucasmeirellesc@hotmail.com>
2022-06-10 20:00:53 -0300

    [FEAT] - implemented a randomize pieces creation

--- EBAC_MobileGame/Assets/Scripts/LevelManager/LevelManager.cs
+++ EBAC_MobileGame/Assets/Scripts/LevelManager/LevelManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
@@ -8,12 +9,26 @@ public class LevelManager : MonoBehaviour
 
     public List<GameObject> Levels;
 
+    [Header("Pieces List")]
+    public List<LevelPieceBase> LevelPiecesMiddle;
+    public List<LevelPieceBase> LevelPiecesStart;
+    public List<LevelPieceBase> LevelPiecesEnd;
+
+    [Header("Pieces' Quantity")]
+    public int pieceNumberMiddle = 5;
+    public int pieceNumberStart = 3;
+    public int pieceNumberEnd = 1;
+
+
     [SerializeField] private int _index;
     private GameObject _currentLevel;
+    private List<LevelPieceBase> _spawnedPieces;
 
+    #region Start / Update
     private void Awake()
     {
-        SpawnNextLevel();
+        //SpawnNextLevel();
+        CreateLevelPieces();
     }
 
     void Update()
@@ -23,7 +38,9 @@ public class LevelManager : MonoBehaviour
             SpawnNextLevel();
         }
     }
+    #endregion
 
+    #region Level
     private void SpawnNextLevel()
     {
         if(_currentLevel != null)
@@ -45,5 +62,49 @@ public class LevelManager : MonoBehaviour
     {
         _index = 0;
     }
+    #endregion
+
+    #region Piece
+    private void CreateLevelPieces()
+    {
+        _spawnedPieces = new List<LevelPieceBase>();
+
+        for (int i = 0; i < pieceNumberStart; i++)
+        {
+            CreateLevelPiece(LevelPiecesStart);
+        }
+
+        for (int i = 0; i < pieceNumberMiddle; i++)
+        {
+            CreateLevelPiece(LevelPiecesMiddle);
+        }
+
+        for (int i = 0; i < pieceNumberEnd; i++)
+        {
+            CreateLevelPiece(LevelPiecesEnd);
+        }
+    }
+
+    private void CreateLevelPiece(List<LevelPieceBase> levelPieces)
+    {
+        // Get next random piece
+        LevelPieceBase piece = levelPieces[Random.Range(0, levelPieces.Count)];
+
+        // Instantiate the new piece
+        LevelPieceBase spawnedPiece = Instantiate(piece, Container);
+
+        // Check if already exist pieces in the list
+        if(_spawnedPieces.Count > 0)
+        {
+            // Get the last piece
+            LevelPieceBase lastPiece = _spawnedPieces.Last();
+
+            spawnedPiece.transform.position = lastPiece.EndPiece.position;
+        }
+
+        // Fix the position of the new piece, conecting it with the last one
+        _spawnedPieces.Add(spawnedPiece);
+    }
+    #endregion
 
 }
---
+++ EBAC_MobileGame/Assets/Scripts/LevelManager/LevelPieceBase.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPieceBase : MonoBehaviour
+{
+    public Transform EndPiece;
+}
